5d6a8119aae157378fd09f87956025cf7117b23b
23garyd <garyding5678biz@gmail.com>
2022-06-06 20:38:35 -0700

    Added person detection. draft version

--- tests/test_ironoak.py
+++ tests/test_ironoak.py
@@ -1,6 +1,17 @@
 
 from ironoak.cli import main
+from ironoak.person import persondetector
+import unittest
 
-
-def test_main():
-    assert main([]) == 0
+class TestMain(unittest.TestCase):
+    def test_person(self):
+        try:
+            result = False
+            det = persondetector.persondetector()
+            while True:
+                result = det.read()             #nonblock read
+                if result:
+                    break
+            self.assertEqual(result, True)      #Passes test if a person is found.
+        except:
+            pass

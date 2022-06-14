daf8d732b0f0753774382a99f4d7d9d043ba5f5d
HavivaB <beaverbear26@gmail.com>
2022-06-13 21:58:54 -0700

    This is my progress so far on trying to get depth XYZ ... still not there, but depth image is working simultanously with apriltag

---
+++ src/ironoak/apriltag_rgb.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+
+
+import cv2
+import depthai as dai
+import time
+
+# new
+newConfig = True
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+camRgb = pipeline.create(dai.node.ColorCamera)
+aprilTag = pipeline.create(dai.node.AprilTag)
+manip = pipeline.create(dai.node.ImageManip)
+
+# new adds
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+stereo = pipeline.create(dai.node.StereoDepth)
+spatialLocationCalculator = pipeline.create(dai.node.SpatialLocationCalculator)
+
+# new
+xoutDepth = pipeline.create(dai.node.XLinkOut)
+xoutSpatialData = pipeline.create(dai.node.XLinkOut)
+xinSpatialCalcConfig = pipeline.create(dai.node.XLinkIn)
+
+xoutAprilTag = pipeline.create(dai.node.XLinkOut)
+xoutAprilTagImage = pipeline.create(dai.node.XLinkOut)
+
+xoutAprilTag.setStreamName("aprilTagData")
+xoutAprilTagImage.setStreamName("aprilTagImage")
+
+# new
+xoutDepth.setStreamName("depth")
+xoutSpatialData.setStreamName("spatialData")
+xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
+
+# Properties
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+
+manip.initialConfig.setResize(480, 270)
+manip.initialConfig.setFrameType(dai.ImgFrame.Type.GRAY8)
+
+aprilTag.initialConfig.setFamily(dai.AprilTagConfig.Family.TAG_36H11)
+
+# new
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+lrcheck = False
+subpixel = False
+stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+stereo.setLeftRightCheck(lrcheck)
+stereo.setSubpixel(subpixel)
+# new Config
+topLeft = dai.Point2f(0.4, 0.4)
+bottomRight = dai.Point2f(0.6, 0.6)
+
+config = dai.SpatialLocationCalculatorConfigData()
+config.depthThresholds.lowerThreshold = 100
+config.depthThresholds.upperThreshold = 10000
+config.roi = dai.Rect(topLeft, bottomRight)  # I added bottom left and top right
+
+spatialLocationCalculator.inputConfig.setWaitForMessage(False)
+spatialLocationCalculator.initialConfig.addROI(config)
+
+# Linking
+aprilTag.passthroughInputImage.link(xoutAprilTagImage.input)
+camRgb.video.link(manip.inputImage)
+manip.out.link(aprilTag.inputImage)
+aprilTag.out.link(xoutAprilTag.input)
+# always take the latest frame as apriltag detections are slow
+aprilTag.inputImage.setBlocking(False)
+aprilTag.inputImage.setQueueSize(1)
+
+# # new
+monoLeft.out.link(stereo.left)
+monoRight.out.link(stereo.right)
+
+spatialLocationCalculator.passthroughDepth.link(xoutDepth.input)
+stereo.depth.link(spatialLocationCalculator.inputDepth)
+
+spatialLocationCalculator.out.link(xoutSpatialData.input)
+xinSpatialCalcConfig.out.link(spatialLocationCalculator.inputConfig)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+    # Output queue will be used to get the mono frames from the outputs defined above
+    manipQueue = device.getOutputQueue("aprilTagImage", 8, False)
+    aprilTagQueue = device.getOutputQueue("aprilTagData", 8, False)
+
+    # new
+    depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+    spatialCalcQueue = device.getOutputQueue(name="spatialData", maxSize=4, blocking=False)
+    spatialCalcConfigInQueue = device.getInputQueue("spatialCalcConfig")
+
+    color = (0, 255, 0)
+
+    startTime = time.monotonic()
+    counter = 0
+    fps = 0
+
+    while (True):
+        inFrame = manipQueue.get()
+
+        # new
+        inDepth = depthQueue.get()  # Blocking call, will wait until a new data has arrived
+
+        depthFrame = inDepth.getFrame()  # depthFrame values are in millimeters in numpy array
+
+        print(depthFrame.ndim)
+        print(depthFrame.shape)
+        print(depthFrame.dtype)
+
+        depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
+        depthFrameColor = cv2.equalizeHist(depthFrameColor)
+        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+
+        counter += 1
+        current_time = time.monotonic()
+        if (current_time - startTime) > 1:
+            fps = counter / (current_time - startTime)
+            counter = 0
+            startTime = current_time
+
+        monoFrame = inFrame.getFrame()
+        frame = cv2.cvtColor(monoFrame, cv2.COLOR_GRAY2BGR)
+        aprilTagData = aprilTagQueue.get().aprilTags
+        for aprilTag in aprilTagData:
+            topLeft = aprilTag.topLeft
+            topRight = aprilTag.topRight
+            bottomRight = aprilTag.bottomRight
+            bottomLeft = aprilTag.bottomLeft
+            print("topLeft is {}, {}".format(topLeft.x, topLeft.y))
+            print("topRight is {}, {}".format(topRight.x, topRight.y))
+            print("bottomRight is {}, {}".format(bottomRight.x, bottomRight.y))
+            print("bottomLeft is {}, {}".format(bottomLeft.x, bottomLeft.y))
+
+            center = (int((topLeft.x + bottomRight.x) / 2), int((topLeft.y + bottomRight.y) / 2))
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+            cv2.line(frame, (int(topLeft.x), int(topLeft.y)), (int(topRight.x), int(topRight.y)), color, 2,
+                     cv2.LINE_AA, 0)
+            cv2.line(frame, (int(topRight.x), int(topRight.y)), (int(bottomRight.x), int(bottomRight.y)), color, 2,
+                     cv2.LINE_AA, 0)
+            cv2.line(frame, (int(bottomRight.x), int(bottomRight.y)), (int(bottomLeft.x), int(bottomLeft.y)), color,
+                     2, cv2.LINE_AA, 0)
+            cv2.line(frame, (int(bottomLeft.x), int(bottomLeft.y)), (int(topLeft.x), int(topLeft.y)), color, 2,
+                     cv2.LINE_AA, 0)
+
+            idStr = "ID: " + str(aprilTag.id)
+            cv2.putText(frame, idStr, center, cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+
+            # new
+            spatialData = spatialCalcQueue.get().getSpatialLocations()
+            for depthData in spatialData:
+                topLeft = aprilTag.topLeft
+                topRight = aprilTag.topRight
+                bottomRight = aprilTag.bottomRight
+                bottomLeft = aprilTag.bottomLeft
+                roi = depthData.config.roi
+                roi = roi.denormalize(width=depthFrameColor.shape[1], height=depthFrameColor.shape[0])
+                xmin = int(roi.topLeft().x)
+                ymin = int(roi.topLeft().y)
+                xmax = int(roi.bottomRight().x)
+                ymax = int(roi.bottomRight().y)
+
+                depthMin = depthData.depthMin
+                depthMax = depthData.depthMax
+
+                fontType = cv2.FONT_HERSHEY_TRIPLEX
+                cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
+
+                cv2.putText(depthFrameColor, f"X: {int(depthData.spatialCoordinates.x)} mm", (xmin + 10, ymin + 20),
+                            fontType, 0.5, 255)
+                cv2.putText(depthFrameColor, f"Y: {int(depthData.spatialCoordinates.y)} mm", (xmin + 10, ymin + 35),
+                            fontType, 0.5, 255)
+                cv2.putText(depthFrameColor, f"Z: {int(depthData.spatialCoordinates.z)} mm", (xmin + 10, ymin + 50),
+                            fontType, 0.5, 255)
+
+        cv2.putText(frame, "Fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4,
+                    (255, 255, 255))
+
+        cv2.imshow("April tag frame", frame)
+
+        # Show the frame
+        cv2.imshow("depth", depthFrameColor)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
